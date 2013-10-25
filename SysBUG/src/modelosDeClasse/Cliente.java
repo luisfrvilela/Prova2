2199da74286dc4aa8fa55f3ff8ce4971ff334627
Luís Filipe Resende Vilela <luisfrvilela@gmail.com>
2013-10-25 15:27:51 -0300

    Commit inicial com classes modelos

---
+++ SysBUG/src/modelosDeClasse/Cliente.java
@@ -0,0 +1,29 @@
+package modelosDeClasse;
+
+public class Cliente extends Pessoa{
+
+	
+	private String numeroDaConta;
+	private String senhaDoBanco;
+	
+	public Cliente(String nome, String telefone, Endereco endereco) {
+		super(nome, telefone, endereco);
+	}
+
+	public String getNumeroDaConta() {
+		return numeroDaConta;
+	}
+
+	public void setNumeroDaConta(String numeroDaConta) {
+		this.numeroDaConta = numeroDaConta;
+	}
+
+	public String getSenhaDoBanco() {
+		return senhaDoBanco;
+	}
+
+	public void setSenhaDoBanco(String senhaDoBanco) {
+		this.senhaDoBanco = senhaDoBanco;
+	}
+	
+}
---
+++ SysBUG/src/modelosDeClasse/ClientePessoaJuridica.java
@@ -0,0 +1,30 @@
+package modelosDeClasse;
+
+public class ClientePessoaJuridica extends Cliente {
+
+	private String cnpj;
+	private String razaoSocial;
+	
+	public ClientePessoaJuridica(String nome, String telefone, Endereco endereco, String cnpj) {
+		super(nome, telefone, endereco);
+		this.cnpj = cnpj;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	
+	
+}
